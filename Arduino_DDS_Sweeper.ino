604ee471e38e42d31c73a31922f3ae5110a1e3af
Matt Allen <matthew.allen@technipfmc.com>
2018-02-14 15:38:17 -0500

    Adding in LCD libraries/functions, AD9850 functions, SoftwareSerial hooks.

--- Arduino_DDS_Sweeper.ino
+++ Arduino_DDS_Sweeper.ino
@@ -1,10 +1,43 @@
 #include <stdint.h>
 #include <TFTv2.h>
 #include <SPI.h>
+#include <SoftwareSerial.h>
+
+//Setup some items
+#define W_CLK 8   // Pin 8 - connect to AD9850 module word load clock pin (CLK)
+#define FQ_UD 9   // Pin 9 - connect to freq update pin (FQ)
+#define DATA 10   // Pin 10 - connect to serial data load pin (DATA)
+#define RESET 11  // Pin 11 - connect to reset pin (RST) 
+#define pulseHigh(pin) {digitalWrite(pin, HIGH); digitalWrite(pin, LOW); }
+#define TX_PIN 3
+#define LED_PIN 13
+#define ANALOG_VOLTAGE_PIN 1
+
+int_fast32_t rx=7025000; // Starting frequency of VFO
+int_fast32_t rx2=1; // variable to hold the updated frequency
+int_fast32_t increment = 10000; // starting VFO update increment in HZ.
+int buttonstate = 0;
+int buttonstate1 = 0;
+int buttonstate2 = 0;
+String hertz = "10 KHz";
+int  hertzPosition = 5;
+byte ones,tens,hundreds,thousands,tenthousands,hundredthousands,millions ;  //Placeholders
+String freq; // string to hold the frequency
+int_fast32_t timepassed = millis(); // int to hold the arduino miilis since startup
 
 float ltx = 0;    // Saved x coord of bottom of needle
 uint16_t osx = 120, osy = 120; // Saved x & y coords
 uint32_t updateTime = 0;       // time for next update
+
+const char *splash =
+" _  __   _____   ____    ______  __    __   _____ \r\n"
+"| |/ /  / ____| |___ \  |  ____| \ \  / /  / ____|\r\n"
+"| ' /  | |        __) | | |__     \ \/ /  | (___  \r\n"
+"|  <   | |       |__ <  |  __|     \  /    \___ \ \r\n"
+"| . \  | |____   ___) | | |____    |  |    ____) |\r\n"
+"|_|\_\  \_____| |____/  |______|   |__|   |_____/ \r\n";
+
+SoftwareSerial lcd = SoftwareSerial(255, TX_PIN);
 
 int old_analog =  -999; // Value last displayed
 int old_digital = -999; // Value last displayed
@@ -25,6 +58,15 @@ void setup()
 void loop()
 {
   if (updateTime <= millis()) {
+
+//    rx = SerialInput();
+
+//    if (rx != rx2)
+//    {  
+//          sendFrequency(rx);
+//          rx2 = rx;
+//    }
+
     updateTime = millis() + 500;
 
     // Read the forawrd and reverse voltages [map to 0-100 scale for plotNeedle()]
@@ -238,4 +280,48 @@ char* numToAscii(double num)
 	strcat(ascii,".");
 	itoa(frac,&ascii[strlen(ascii)],10);  // put the frac after the decimal
 	return ascii;
+}
+
+// frequency calc from datasheet page 8 = <sys clock> * <frequency tuning word>/2^32
+void sendFrequency(double frequency) 
+{  
+  int32_t freq = frequency * 4294967295/125000000;  // note 125 MHz clock on 9850.  You can make 'slight' tuning variations here by adjusting the clock frequency.
+  for (int b=0; b<4; b++, freq>>=8) {
+    tfr_byte(freq & 0xFF);
+  }
+  tfr_byte(0x000);   // Final control byte, all 0 for 9850 chip
+  pulseHigh(FQ_UD);  // Done!  Should see output
+}
+
+// transfers a byte, a bit at a time, LSB first to the 9850 via serial DATA line
+void tfr_byte(byte data)
+{
+  for (int i=0; i<8; i++, data>>=1) {
+    digitalWrite(DATA, data & 0x01);
+    pulseHigh(W_CLK);   //after each bit sent, CLK is pulsed high
+  }
+}
+
+int SerialInput()
+{
+    char incomingByte;
+    while (Serial.available() > 0) 
+    {
+      incomingByte = Serial.read();
+      if (incomingByte == '\n') break;   // exit after newline char
+      if (incomingByte == -1) continue;  // if no chars in buffer...Serial.read returns -1...
+      rx *= 1000;
+      rx += (incomingByte - 48);
+    }
+    return rx;
+}
+
+void setCursor(uint8_t row, uint8_t col)
+{
+    int position;
+    if(row==0) position = 128;
+    else if (row==1) position = 148;
+    
+    position = position + col;
+    lcd.write(position);    
 }
